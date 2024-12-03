62c46c1fc94079bbd40732e90d39a0d0fbcf7186
Aviel <aviell1100611@gmail.com>
2024-12-03 19:39:15 +0200

    Routing option added when needed

---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <div>Checking</div>,
+      },
+    ],
+  },
+]);
