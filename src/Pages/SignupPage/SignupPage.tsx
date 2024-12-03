6853abd21da6fc10179e32e12e99986dbe3102b4
Aviel <aviell1100611@gmail.com>
2024-12-04 01:44:54 +0200

    Signup left side designed - without mobile compatibility

--- src/Pages/SignupPage/SignupPage.tsx
+++ src/Pages/SignupPage/SignupPage.tsx
@@ -1,27 +1,33 @@
 const SignupPage = () => {
   return (
-    <div className="flex h-full w-full text-white">
-      <div className="flex w-1/2 justify-center bg-signup-bg bg-cover bg-center p-8">
-        <div className="absolute top-0 left-0">
+    <div className="flex h-full w-full">
+      <div className="flex flex-col w-1/2 bg-signup-bg bg-cover bg-center p-8 text-white flex-1 justify-center items-center">
+        <div className="h-1/4">
           <img
             src="/public/assets/logo/logo-white.png"
-            className="w-64 h-auto m-8"
+            className="absolute w-64 h-auto top-0 left-0 m-12"
             alt="AvlHealth Logo"
           />
         </div>
-
-        <div className="flex flex-col items-center justify-center">
-          <img
-            src="/public/assets/items/signup-picture.png"
-            className="h-[32rem] w-auto"
-            alt="signup-picture"
-          />
-          <div className="flex flex-col">
-            <h1 className="text-3xl text-center">Lorem Ipsum, easy and Fast</h1>
-            <p>
-              Overlord help you wset fsdfsd, fdsf f sdfsd fsd fsd,f sdf sd,f
-              sdfsdfsdf sdfdsf sdfsd fsdfsd fds, fsdf fsd
-            </p>
+        <div className="h-3/4">
+          <div className="flex flex-col items-center justify-center">
+            <img
+              src="/public/assets/items/signup-picture.png"
+              className="h-[32rem] w-auto"
+              alt="signup-picture"
+            />
+            <div className="flex flex-col gap-4 text-center justify-center">
+              <h1 className="text-5xl font-semibold text-center">
+                Go beyond, and even more
+              </h1>
+              <p className="text-xl text-center">
+                AvlHealth is your ultimate companion for achieving your weight
+                goals and leading a healthier lifestyle.
+                <br /> With our app, you can set personalized weight targets,
+                track your daily meal calories, and monitor your progress
+                through detailed and visually appealing graphs.
+              </p>
+            </div>
           </div>
         </div>
       </div>
